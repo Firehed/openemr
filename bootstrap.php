e492e96958592add09f01603d862711456ec3037
Eric Stern <eric@ericstern.com>
2025-12-16 09:16:19 -0800

    Clarify some comments

--- bootstrap.php
+++ bootstrap.php
@@ -11,10 +11,16 @@ define('APPLICATION_BOOTSTRAP_INCLUDED', true);
 
 // Application bootstrapping
 //
-// This performs some common setup tasks that should affect all requests as
-// well as CLI interactions:
-// - Server/environment checks
+// This performs some common setup tasks that should affect all paths through
+// the application:
+// - Web requests
+// - CLI tools
+// - Automated tests
+// As such, this bootstrap should make no assumptions about the runtime
 // - Composer/autoloading
+
+// In the future, this will handle other common tasks.
+// - Server/environment checks
 // - Error handling
 // - App config/service container
 
@@ -22,17 +28,17 @@ define('APPLICATION_BOOTSTRAP_INCLUDED', true);
 /**
  * Future scope:
 
-// Make everything relative to the repo root
+// Make paths relative to the repo root (there's probably a _lot_ of code that
+// this would break)
 chdir(__DIR__);
 
-// Set some
+// Set some reasonable defaults around error handling and logging
 date_default_timezone_set('UTC');
 ini_set('display_errors', '0');
 ini_set('display_startup_errors', '0');
 ini_set('error_log', '/dev/stdout'); // Docker wants logs written to stdout
 ini_set('error_reporting', (string)E_ALL);
 ini_set('log_errors', '1');
-
  */
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -41,12 +47,13 @@ require_once __DIR__ . '/vendor/autoload.php';
  * Future scope:
 
 if (class_exists(Dotenv\Dotenv::class)) {
-    // Load dotenv. This should only really be used for dev environments,
-    // though perhaps it makes sense for all envs for OpenEMR.
+    // Load dotenv. This should only really be used for dev environments (hence
+    // being conditionally included, though perhaps it makes sense for all envs
+    // for OpenEMR). See current usage in interface/globals.php.
 }
 
 // Initialize and make accessible a DI/service container
-// $config = require 'di_init.php';
+// $config = require 'some-file-providing-service-container.php';
 
 // Set up error handling
 $handler = $config->get(ErrorHandler::clas);
